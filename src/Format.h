c620cbe73dd2f28256095420395023bb2998db20
Rebecca Hom <rhom0505@gmail.com>
2015-12-14 16:44:08 -0800

    added in prompt questions

--- src/Format.h
+++ src/Format.h
@@ -4,3 +4,10 @@ using namespace std;
 
 //  string zero(unsigned) - Returns a leading zero for date
 string zero(unsigned num);
+
+//  unsigned prompt(string&, unsigned) - checks the user's choice against 
+//      the number of choices available.
+unsigned prompt(string input, unsigned max);
+
+//  unsigned promptNav(unsigned) - checks the user's navigation choice
+unsigned promptNav(unsigned max);
