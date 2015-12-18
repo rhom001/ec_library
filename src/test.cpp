a1935f1d3a0355b6dd6a10cc03dc7889fb2ab80a
Rebecca Hom <rhom0505@gmail.com>
2015-12-18 12:55:31 -0800

    added test case for User class

--- src/test.cpp
+++ src/test.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+    /*
     //  Tests the Date class
     cout << "Date class" << endl;
     Date* d = new Date(12, 27, 2007);
@@ -48,5 +49,34 @@ int main()
     r2->remove();
     cout << "The role has been cleared!" << endl;
     r2->print();
+    cout << endl;
+    */
+    
+    //  Tests the User class
+    cout << "User class" << endl;
+    string username = "Guest";
+    string password = "password";
+    cout << "User: " << username << endl
+        << "Password: " << password << endl;
+    cout << endl;
+
+    User* u1 = new User(username, password, true, false);
+    u1->print();
+    cout << endl;
+
+    cout << "Adding a new User..." << endl;
+    User* u2 = new User();
+    u2->add();
+    u2->print();
+    cout << endl;
+
+    cout << "Time to remove the second user!" << endl;
+    u2->remove();
+    u2->print();
+    cout << endl;
+
+    cout << "Let's change the first user" << endl;
+    u1->modify();
+    u1->print();
     return 0;
 }
